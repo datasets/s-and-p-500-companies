33327e7895bf635840f5f5fc43305e3a63eec035
Rufus Pollock <rufus.pollock@okfn.org>
2013-10-05 13:44:01 +0100

    [scripts][s]: convert constituents.sh shell code into python as part of constituents.py.
    Had encountered an error due to new line in a field (caused by addition of 20th century fox with a comma in its name) so thought worth converting to make this more robust.

--- scripts/constituents.py
+++ scripts/constituents.py
@@ -1,26 +1,55 @@
 import csv
 import os
+import urllib
+import dataconverters.xls
 
 # first run the extract
-os.system('. scripts/constituents.sh')
-
+# os.system('. scripts/constituents.sh')
+source = "http://us.spindices.com/idsexport/file.xls?hostIdentifier=48190c8c-42c4-46af-8d1a-0cd5db894797&selectedModule=Constituents&selectedSubModule=ConstituentsFullList&indexId=340"
+xlspath = 'cache/constituents.xls'
 path = 'data/constituents.csv'
 
-existingr = csv.reader(open(path))
-header = existingr.next()
+def execute():
+    # urllib.urlretrieve(source, xlspath)
 
-# { symbol: row } dict
-existing = dict([ [row[0], row] for row in existingr ])
+    existingr = csv.reader(open(path))
+    header = existingr.next()
+    # { symbol: row } dict
+    existing = dict([ [row[0], row] for row in existingr ])
 
-writer = csv.writer(open(path, 'w'), lineterminator='\n')
-writer.writerow(header)
+    # have to set explicit encoding as otherwise bad encoding error
+    # ERROR *** codepage 21010 -> encoding 'unknown_codepage_21010' -> LookupError: unknown encoding: unknown_codepage_21010
+    records, metadata = dataconverters.xls.parse(open(xlspath),
+                excel_type='xls',
+                encoding='utf8'
+                )
 
-for row in csv.reader(open('cache/constituents.csv')):
-    # BRK/B or BRK.B => BRK-B (this is what yahoo needs)
-    symbol = row[0].replace('/', '-')
-    symbol = row[0].replace('.', '-')
-    if symbol in existing:
-        writer.writerow(existing[symbol])
-    else:
-        writer.writerow(row + [''])
+    fields = [ f['id'] for f in metadata['fields'] ]
+    # order is company name | symbol
+    records = [ [ x[fields[0]], fixsymbol(x[fields[1]]) ] for x in records ]
+    # chop header and footer bumpf
+    records = records[6:]
+    records = records[:-4]
+    records.sort(key=lambda s: s[0].lower())
+
+    writer = csv.writer(open(path, 'w'), lineterminator='\n')
+    writer.writerow(header)
+    for row in records:
+        symbol = row[1]
+        # faff around so that we preserve the existing sector info
+        # have to add new sector info by hand ...
+        if symbol in existing:
+            writer.writerow(existing[symbol])
+        else:
+            print('New entry in list: %s' % row)
+            row.reverse()
+            writer.writerow(row + [''])
+
+# BRK/B or BRK.B => BRK-B (this is what yahoo needs)
+def fixsymbol(symbol):
+    symbol = symbol.replace('/', '-')
+    symbol = symbol.replace('.', '-')
+    return symbol
+
+execute()
 
