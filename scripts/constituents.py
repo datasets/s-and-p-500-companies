1c45899dd38e7bb2ea2795b23a94cbf067072940
Rufus Pollock <rufus.pollock@okfn.org>
2018-04-02 22:21:56 +0200

    [scripts/constituents][m]: refactor to retrieve WP page in python rather than in makefile.
    * Refactor to have separate methods i.e. extract and process
    * Add retrieve method and cache file in tmp directory

--- scripts/constituents.py
+++ scripts/constituents.py
@@ -2,32 +2,54 @@ from bs4 import BeautifulSoup
 import csv
 from os import mkdir
 from os.path import exists, join
+import urllib.request as request
+
+
 datadir = join('..', 'data')
 if not exists(datadir):
     mkdir(datadir)
-source_page = open('List_of_S%26P_500_companies.html').read()
-soup = BeautifulSoup(source_page, 'html.parser')
-table = soup.find("table", { "class" : "wikitable sortable" })
 
-# Fail now if we haven't found the right table
-header = table.findAll('th')
-if header[0].string != "Ticker symbol" or header[1].string != "Security":
-    raise Exception("Can't parse wikipedia's table!")
+if not exists('tmp'):
+    mkdir('tmp')
 
-# Retreive the values in the table
-records = []
-rows = table.findAll('tr')
-for row in rows:
-    fields = row.findAll('td')
-    if fields:
-        symbol = fields[0].string
-        name = fields[1].string
-        sector = fields[3].string
-        records.append([symbol, name, sector])
+source = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
+cache = join('tmp', 'List_of_S%26P_500_companies.html')
 
-header = ['Symbol', 'Name', 'Sector']
-writer = csv.writer(open('../data/constituents.csv', 'w'), lineterminator='\n')
-writer.writerow(header)
-# Sorting ensure easy tracking of modifications
-records.sort(key=lambda s: s[1].lower())
-writer.writerows(records)    
+def retrieve():
+    request.urlretrieve(source, cache)
+
+def extract():
+    source_page = open(cache).read()
+    soup = BeautifulSoup(source_page, 'html.parser')
+    table = soup.find("table", { "class" : "wikitable sortable" })
+
+    # Fail now if we haven't found the right table
+    header = table.findAll('th')
+    if header[0].string != "Ticker symbol" or header[1].string != "Security":
+        raise Exception("Can't parse wikipedia's table!")
+
+    # Retreive the values in the table
+    records = []
+    rows = table.findAll('tr')
+    for row in rows:
+        fields = row.findAll('td')
+        if fields:
+            symbol = fields[0].string
+            name = fields[1].string
+            sector = fields[3].string
+            records.append([symbol, name, sector])
+
+    header = ['Symbol', 'Name', 'Sector']
+    writer = csv.writer(open('../data/constituents.csv', 'w'), lineterminator='\n')
+    writer.writerow(header)
+    # Sorting ensure easy tracking of modifications
+    records.sort(key=lambda s: s[1].lower())
+    writer.writerows(records)    
+
+def process():
+    retrieve()
+    extract()
+
+if __name__ == '__main__':
+    process()
+
