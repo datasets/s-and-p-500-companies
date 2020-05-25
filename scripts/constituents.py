557fe427a891fce6777fc2c22ff6769211b8ecc1
Sébastien Lavoie <sgdlavoie@gmail.com>
2020-05-25 09:12:03 -0500

    [github-actions][s]: Set automated workflow
      * Switch from Travis CI to GitHub Actions (like other datasets).
      * Fix issue with wrong attribute (`string` -> `text`).
      * Reformat script with Black.
      * Change relative paths to work from root directory.

--- scripts/constituents.py
+++ scripts/constituents.py
@@ -5,59 +5,64 @@ from os.path import exists, join
 import urllib.request as request
 
 
-datadir = join('..', 'data')
+datadir = "data"
 if not exists(datadir):
     mkdir(datadir)
 
-if not exists('tmp'):
-    mkdir('tmp')
+if not exists("scripts/tmp"):
+    mkdir("scripts/tmp")
 
 source = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
-cache = join('tmp', 'List_of_S%26P_500_companies.html')
+cache = join("scripts/tmp", "List_of_S%26P_500_companies.html")
+
 
 def retrieve():
     request.urlretrieve(source, cache)
 
+
 def extract():
     source_page = open(cache).read()
-    soup = BeautifulSoup(source_page, 'html.parser')
-    table = soup.find("table", { "class" : "wikitable sortable" })
+    soup = BeautifulSoup(source_page, "html.parser")
+    table = soup.find("table", {"class": "wikitable sortable"})
 
     # Fail now if we haven't found the right table
-    header = table.findAll('th')
+    header = table.findAll("th")
     if header[0].text.rstrip() != "Symbol" or header[1].string != "Security":
-        raise Exception("Can't parse wikipedia's table!")
+        raise Exception("Can't parse Wikipedia's table!")
 
-    # Retreive the values in the table
+    # Retrieve the values in the table
     records = []
     symbols = []
-    rows = table.findAll('tr')
+    rows = table.findAll("tr")
     for row in rows:
-        fields = row.findAll('td')
+        fields = row.findAll("td")
         if fields:
             symbol = fields[0].text.rstrip()
             # fix as now they have links to the companies on WP
-            name = fields[1].string.replace(',', '')
-            sector = fields[3].string.rstrip()
+            name = fields[1].text.replace(",", "")
+            sector = fields[3].text.rstrip()
             records.append([symbol, name, sector])
-            symbols.append(symbol + '\n')
+            symbols.append(symbol + "\n")
 
-    header = ['Symbol', 'Name', 'Sector']
-    writer = csv.writer(open('../data/constituents.csv', 'w'), lineterminator='\n')
+    header = ["Symbol", "Name", "Sector"]
+    writer = csv.writer(
+        open("./data/constituents.csv", "w"), lineterminator="\n"
+    )
     writer.writerow(header)
     # Sorting ensure easy tracking of modifications
     records.sort(key=lambda s: s[1].lower())
     writer.writerows(records)
 
-    with open('../data/constituents_symbols.txt', 'w') as f:
+    with open("./data/constituents_symbols.txt", "w") as f:
         # Sorting ensure easy tracking of modifications
         symbols.sort(key=lambda s: s[0].lower())
         f.writelines(symbols)
+
 
 def process():
     retrieve()
     extract()
 
-if __name__ == '__main__':
-    process()
 
+if __name__ == "__main__":
+    process()
